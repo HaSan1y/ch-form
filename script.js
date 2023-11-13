30edbfc188122c22d7001ff6889beaafdc9cf1fc
Hasan Yildirim <endplay_xp@hotmail.com>
2023-11-13 02:22:15 +0100

    finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let x,y;
+let email, y;
 
 function validateEmail(email) {
   console.log("s");
@@ -6,16 +6,15 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-
 function showMainDiv() {
   email = document.getElementById("name").value;
   if (!validateEmail(email)) {
     alert("Invalid email address!");
     return false;
   } else {
-    x = document.getElementById("name").value;
+    email = document.getElementById("name").value;
     y = document.getElementById("asdf");
-    y.innerText = x;
+    y.innerText = email;
 
     document.querySelector(".main").style.display = "block";
     document.querySelector("main").style.display = "none";
